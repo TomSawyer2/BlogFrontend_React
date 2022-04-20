c0b4dcc3e3938cfe0fddbeb9d0b35eaeb7cc8691
TomSawyer2 <6272867@qq.com>
2022-04-20 10:35:17 +0800

    🐞 fix(index): fix scroll bar in index page

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,23 +1,9 @@
 import styles from './index.css';
 import ParallaxBanner from '@/components/ParallaxBanner';
-// import NavigationBar from '@/components/NavigationBar';
-// import MainArticles from '@/components/MainArticles';
-// import { useEffect, useState } from 'react';
 
 export default function IndexPage(this: any) {
-  // const [offsetTop, setOffsetTop] = useState<number>(0);
-  // useEffect(() => {
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => {};
-  // }, []);
-
-  // const handleScroll = () => {
-  //   setOffsetTop(window.scrollY);
-  // };
-
   return (
     <>
-      {/* <NavigationBar offsetTop={offsetTop} /> */}
       <ParallaxBanner className={styles} />
     </>
   );
