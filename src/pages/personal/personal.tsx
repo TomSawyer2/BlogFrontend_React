ff1c07dd17a7b89c349d9ba32b620ed3b5437f21
TomSawyer2 <6272867@qq.com>
2022-04-22 20:31:29 +0800

    ✨ feat(personalpage): add default personal page

--- src/pages/personal/personal.tsx
+++ src/pages/personal/personal.tsx
@@ -1,7 +1,20 @@
+import BubblePink from '@/components/BubblePink/BubblePink';
+import Wave from '@/components/Wave/Wave';
 import styles from './personal.less';
 
 const Personal: React.FC = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.avatar}>
+          <BubblePink />
+        </div>
+        <div className={styles.waveContainer}>
+          <Wave />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Personal;
